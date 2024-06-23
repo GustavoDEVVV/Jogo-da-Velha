547b50974e13c3a33eff3a634a0f3775c0067d68
Gustavo Domingues <142946339+GustavoDEVVV@users.noreply.github.com>
2024-06-23 12:55:32 -0300

    alteração

--- projeto/index.js
+++ projeto/index.js
@@ -3,10 +3,10 @@ let boxes = document.querySelectorAll(".box");
 let turn = "X";
 let isGameOver = false;
 
-boxes.forEach(e =>{
+boxes.forEach(e => {
     e.innerHTML = ""
-    e.addEventListener("click", ()=>{
-        if(!isGameOver && e.innerHTML === ""){
+    e.addEventListener("click", () => {
+        if (!isGameOver && e.innerHTML === "") {
             e.innerHTML = turn;
             cheakWin();
             cheakDraw();
@@ -15,39 +15,39 @@ boxes.forEach(e =>{
     })
 })
 
-document.querySelector(".bg").addEventListener("click", function() {
+document.querySelector(".bg").addEventListener("click", function () {
     changeTurn();
 });
 
 
-function changeTurn(){
-    if(turn === "X"){
+function changeTurn() {
+    if (turn === "X") {
         turn = "0";
         document.querySelector(".bg").style.left = "85px";
     }
-    else{
+    else {
         turn = "X";
         document.querySelector(".bg").style.left = "0";
     }
 }
 
-function cheakWin(){
+function cheakWin() {
     let winConditions = [
-        [0,1,2], [3,4,5], [6,7,8],
-        [0,3,6], [1,4,7], [2,5,8],
-        [0,4,8], [2,4,6]
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
     ]
-    for(let i = 0; i<winConditions.length; i++){
+    for (let i = 0; i < winConditions.length; i++) {
         let v0 = boxes[winConditions[i][0]].innerHTML;
         let v1 = boxes[winConditions[i][1]].innerHTML;
-        let v2 = boxes[winConditions[i][2]].innerHTML; 
-        
-        if(v0 != "" && v0 === v1 && v0 === v2){
+        let v2 = boxes[winConditions[i][2]].innerHTML;
+
+        if (v0 != "" && v0 === v1 && v0 === v2) {
             isGameOver = true;
             document.querySelector("#results").innerHTML = turn + "   Venceu";
             document.querySelector("#play-again").style.display = "inline";
 
-            for(j = 0; j<3; j++){
+            for (j = 0; j < 3; j++) {
                 boxes[winConditions[i][j]].style.backgroundColor = "#08D9D6"
                 boxes[winConditions[i][j]].style.color = "#000"
             }
@@ -55,14 +55,14 @@ function cheakWin(){
     }
 }
 
-function cheakDraw(){
-    if(!isGameOver){
+function cheakDraw() {
+    if (!isGameOver) {
         let isDraw = true;
-        boxes.forEach(e =>{
-            if(e.innerHTML === "") isDraw = false;
+        boxes.forEach(e => {
+            if (e.innerHTML === "") isDraw = false;
         })
 
-        if(isDraw){
+        if (isDraw) {
             isGameOver = true;
             document.querySelector("#results").innerHTML = "Empate";
             document.querySelector("#play-again").style.display = "inline"
@@ -70,14 +70,14 @@ function cheakDraw(){
     }
 }
 
-document.querySelector("#play-again").addEventListener("click", ()=>{
+document.querySelector("#play-again").addEventListener("click", () => {
     isGameOver = false;
     turn = "X";
     document.querySelector(".bg").style.left = "0";
     document.querySelector("#results").innerHTML = "";
     document.querySelector("#play-again").style.display = "none";
 
-    boxes.forEach(e =>{
+    boxes.forEach(e => {
         e.innerHTML = "";
         e.style.removeProperty("background-color");
         e.style.color = "#fff"
